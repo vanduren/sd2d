b16cc05c067b7e1eb542e134ecdf04c0e1bef58c
dujo <dujoroc@gmail.com>
2024-02-21 09:28:09 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="bg-red-300">Hallo</div>
+  <div class="bg-blue-300">andere tekst</div>
+</body>
+</html>
